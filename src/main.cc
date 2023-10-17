41de5eb080066b60192ed605865ed0f8ecbdd289
Darwin David <davidcastroastudillo20@gmail.com>
2023-10-17 17:06:05 -0500

    -

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <drogon/drogon.h>
 #include <src/database/database.hxx>
 #include <src/utils/crypto/crypto.hxx>
+#include <fmt/format.h>
 
 using namespace api::v1;
 
@@ -10,10 +11,9 @@ int main() {
     MongoDb::init();
 
     std::string pwd = "password";
+    std::string encryptPwd = fmt::format("78F3F7C140D6735F95921E00048D23C5AF6E2865F2C42F4C5CD6F4DB4FBE5515");
 
-    //LOG_DEBUG << Crypto::encrypt(pwd);
-
-    //LOG_DEBUG << drogon::utils::getUuid();
+    LOG_DEBUG << Crypto::compare(pwd, encryptPwd);
 
     drogon::app().run();
     return 0;
