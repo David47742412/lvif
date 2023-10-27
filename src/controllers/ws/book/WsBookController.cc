42e702320772dfa739c14f07e106d8ccfdfee2c2
Darwin David <davidcastroastudillo20@gmail.com>
2023-10-26 19:02:06 -0500

    -

--- src/controllers/ws/book/WsBookController.cc
+++ src/controllers/ws/book/WsBookController.cc
@@ -4,21 +4,30 @@ using namespace api::v1;
 
 void WsBookController::handleNewMessage(const WebSocketConnectionPtr &wsConnPtr, std::string &&message,
                                         const WebSocketMessageType &type) {
-    if (type == WebSocketMessageType::Pong) return;
+    try {
+        if (type == WebSocketMessageType::Pong) return;
 
 
-    _room.publish("book", message);
+
+        _room.publish("book", BookService::find().toStyledString());
+    } catch (const std::runtime_error &ex) {
+        LOG_DEBUG << ex.what();
+    }
 }
 
 void WsBookController::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr &wsConnPtr) {
-    wsConnPtr->send(BookService::find().toStyledString());
-    Subscriber subscriber;
-    subscriber.id = _room.subscribe("book",
-                                    [wsConnPtr](const std::string &topic, const std::string &message) {
+    try {
+        wsConnPtr->send(BookService::find().toStyledString());
+        Subscriber subscriber;
+        subscriber.id = _room.subscribe("book",
+                                        [wsConnPtr](const std::string &topic, const std::string &message) {
 
-                                        wsConnPtr->send(message);
-                                    });
-    wsConnPtr->setContext(std::make_shared<Subscriber>(std::move(subscriber)));
+                                            wsConnPtr->send(message);
+                                        });
+        wsConnPtr->setContext(std::make_shared<Subscriber>(std::move(subscriber)));
+    } catch (const std::runtime_error &ex) {
+        LOG_DEBUG << ex.what();
+    }
 }
 
 void WsBookController::handleConnectionClosed(const WebSocketConnectionPtr &wsConnPtr) {
